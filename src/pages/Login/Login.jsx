cc34485dcb369d4ee3dc5dc92e902861793dbf90
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2022-07-09 09:21:11 +0700

    change from loginSlice to navbarSlice

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,26 @@
 import React, { useState, useEffect } from 'react'
-import {SignIn, SignUp} from "../../components"
-import { useSelector } from 'react-redux'
+import { SignIn, SignUp } from "../../components"
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
-import {HiOutlineArrowNarrowLeft} from "react-icons/hi" 
+import { HiOutlineArrowNarrowLeft } from "react-icons/hi"
 
 
 import "./Login.sass"
+import { setCurrentPage } from '../../features/navbar/navbarSlice'
 
 
 const Login = () => {
-    const {form} = useSelector((store) => store.login)
-    console.log(form)
+  const { form } = useSelector((store) => store.login)
+  const dispatch = useDispatch();
   return (
     <div className='login'>
-      <Link to = "/" className="back-btn">
-        <HiOutlineArrowNarrowLeft/>
+      <Link to="/" className="back-btn" onClick={() => dispatch(setCurrentPage("home"))}>
+        <HiOutlineArrowNarrowLeft />
         <h5>Back to Home</h5>
       </Link>
-        <div className="form-container">
-            {form === "signin" ? <SignIn/> : <SignUp/>}
-        </div>
+      <div className="form-container">
+        {form === "signin" ? <SignIn /> : <SignUp />}
+      </div>
     </div>
   )
 }
