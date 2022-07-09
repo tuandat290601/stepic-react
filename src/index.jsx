21a8f64707728bccb4555e1906d47465b843be6f
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2022-07-09 09:16:52 +0700

    add Scroll Button

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home, Login, Game, GameDetail, Payment, Error } from "./pages";
 
 import { store } from './store';
 import { Provider } from "react-redux";
-import { Navbar } from "./components";
+import { Navbar, ScrollButton } from "./components";
+
+import { useSelector } from "react-redux/es/exports";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path = "/" element = {<Home/>}/>
-        <Route path = "/login" element = {<Login/>}/>
-        <Route path = "/game" element = {<Game/>}/>
-        <Route path = "/game/:id" element = {<GameDetail/>}/>
-        <Route path = "/payment" element = {<Payment/>}/>
-        <Route path = "*" element = {<Error/>}/>
-      </Routes>
-      {/* <Footer/> */}
+        <Navbar />
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/game/:id" element={<GameDetail />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        {/* <Footer/> */}
+        <ScrollButton />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
