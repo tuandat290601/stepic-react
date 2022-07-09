e8d5b8400e4648d336333051bc476cd652c1f1f9
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2022-07-09 09:18:16 +0700

    replace banner data (static -> api)

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,138 +1,103 @@
 import React, { useEffect, useState } from 'react'
 import "./Banner.sass"
 
-import axios from 'axios';
-import API from '../../common/API/API';
+
+import API from "../../common/API/API"
 
 import { BsArrowRight } from "react-icons/bs";
-
-// const list = [
-//     {
-//         id: 0,
-//         img: "https://cdn2.unrealengine.com/egs-fall-guys-halo-carousel-desktop-1248x702-fc2c1f2f1d07.jpg?h=1080&resize=1&w=1920",
-//         logo: "https://cdn2.unrealengine.com/egs-saints-row-gold-ed-carousel-logo-500x309-44fce11ba866.png",
-//         name: "Fall guys",
-//         desc: "Reclaim the Dwarven homeland of Moria™. The only survival crafting video game set in the Fourth Age of Middle-earth™.",
-//         price: "199.00$",
-//         old_price: "200.00$",
-//         status: "comming soon"
-//     },
-//     {
-//         id: 1,
-//         img: "https://cdn2.unrealengine.com/egs-saints-row-gold-ed-carousel-desktop-1248x702-b42ef9052908.jpg?h=1080&resize=1&w=1920",
-//         logo: "https://cdn2.unrealengine.com/egs-saints-row-gold-ed-carousel-logo-500x309-44fce11ba866.png",
-//         name: "Fall guys",
-//         desc: "Reclaim the Dwarven homeland of Moria™. The only survival crafting video game set in the Fourth Age of Middle-earth™.",
-//         price: "199.00$",
-//         old_price: "200.00$",
-//         status: "comming soon"
-//     },
-//     {
-//         id: 2,
-//         img: "https://cdn2.unrealengine.com/egs-lotr-return-to-moria-carousel-desktop-1280x702-8737f30f4278.jpg?h=1080&resize=1&w=1920",
-//         logo: "https://cdn2.unrealengine.com/egs-saints-row-gold-ed-carousel-logo-500x309-44fce11ba866.png",
-//         name: "Fall guys",
-//         desc: "Reclaim the Dwarven homeland of Moria™. The only survival crafting video game set in the Fourth Age of Middle-earth™.",
-//         price: "199.00$",
-//         old_price: "200.00$",
-//         status: "comming soon"
-//     },
-//     {
-//         id: 3,
-//         img: "https://cdn2.unrealengine.com/egs-fall-guys-halo-carousel-desktop-1248x702-fc2c1f2f1d07.jpg?h=1080&resize=1&w=1920",
-//         logo: "https://cdn2.unrealengine.com/egs-saints-row-gold-ed-carousel-logo-500x309-44fce11ba866.png",
-//         name: "Fall guys",
-//         desc: "Reclaim the Dwarven homeland of Moria™. The only survival crafting video game set in the Fourth Age of Middle-earth™.",
-//         price: "199.00$",
-//         old_price: "200.00$",
-//         status: "comming soon"
-//     }
-// ]
 
 const Banner = () => {
     const [currentBanner, setCurrentBanner] = useState(0)
     const [list, setList] = useState([])
     const [mainBanner, setMainBanner] = useState(null)
-    const assignData = (data) => {
+
+    const assignData = async (data) => {
         setList(data)
+        setMainBanner(data[0].gameImage)
     }
+
     const handleBanner = (index) => {
         setCurrentBanner(index)
         setMainBanner(list[index].gameImage)
     }
 
-
     const getMovie = async () => {
         await API.get("/product").then(res => {
             assignData(res.data)
-        }).then(() => {
-            handleBanner(0)
+            console.log(res.data)
+        }).catch(err => {
+            console.log(err)
         })
     }
 
     useEffect(() => {
-        getMovie()
+        getMovie();
     }, [])
 
-    // useEffect(() => {
-    //     const timeout = setTimeout(() => {
-    //         if (currentBanner === list.length - 1) {
-    //             handleBanner(0)
-    //         }
-    //         else {
-    //             handleBanner(currentBanner + 1)
-    //         }
-    //     }, 2500)
-    //     return () => {
-    //         clearTimeout(timeout)
-    //     }
-    // })
+    useEffect(() => {
+        if (list) {
+            const timeout = setTimeout(() => {
+                if (currentBanner === list.length - 1) {
+                    handleBanner(0)
+                }
+                else {
+                    handleBanner(currentBanner + 1)
+                }
+            }, 2500)
+            return () => {
+                clearTimeout(timeout)
+            }
+        }
+    })
     return (
-        <section className='banner-container'>
-            <div className="banner-content">
+        <>
+            {list.length !== 0 ? <section className='banner-container' >
+                <div className="banner-content" >
 
-                <div className="main-banner-container">
-                    <img className='main-banner-img' src={mainBanner} alt="" />
-                    <div className="main-banner-blur"></div>
-                    <div className="main-banner-info">
-                        <div className="main-banner-logo">
-                            <img src={list[currentBanner].logoImage} alt="" />
-                        </div>
-                        <p>{list[currentBanner].status}</p>
-                        <p>{list[currentBanner].description}</p>
-                        <div className="price">
-                            {list[currentBanner].old_price !== "" ? <>
-                                <p className='old-price'>{list[currentBanner].old_price} </p> <BsArrowRight />
-                            </> : null}
-                            <h5 className='new-price'>${list[currentBanner].price}</h5>
-                        </div>
-                        <div className="main-banner-btn-container">
-                            <button className='main-banner-btn blue-background'>Add to Cart</button>
-                            <button className='main-banner-btn orange-background'>Buy now</button>
+                    <div className="main-banner-container">
+                        <img className='main-banner-img' src={mainBanner} alt="" />
+                        <div className="main-banner-blur"></div>
+                        <div className="main-banner-info">
+                            <div className="main-banner-logo">
+                                <img src={list[currentBanner].logoImage} alt="" />
+                            </div>
+                            <p>{list[currentBanner].status}</p>
+                            <p>{list[currentBanner].description}</p>
+                            <div className="price">
+                                {list[currentBanner].discount !== 0 ? <>
+                                    <p className='old-price'>${list[currentBanner].price} </p> <BsArrowRight />
+                                </> : null}
+
+                                <h5 className='new-price'>${list[currentBanner].price}</h5>
+                            </div>
+                            <div className="main-banner-btn-container">
+                                <button className='main-banner-btn blue-background'>Add to Cart</button>
+                                <button className='main-banner-btn orange-background'>Buy now</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <ul className="sub-banner-list">
-                    {list.map((item, index) => {
-                        const { id, gameImage } = item
-                        if (index !== currentBanner) {
-                            return <li style={{ filter: "grayscale(1)" }} className='sub-banner-container' key={id} onClick={() => {
-                                handleBanner(index)
-                            }}>
-                                <img className='sub-banner-img' src={gameImage} alt="" />
-                            </li>
-                        }
-                        else {
-                            return <li className='sub-banner-container' key={id} onClick={() => {
-                                handleBanner(index)
-                            }}>
-                                <img className='sub-banner-img' src={gameImage} alt="" />
-                            </li>
-                        }
-                    })}
-                </ul>
-            </div>
-        </section>
+                    <ul className="sub-banner-list">
+                        {list.map((item, index) => {
+                            const { id, gameImage } = item
+                            if (index !== currentBanner) {
+                                return <li style={{ filter: "grayscale(1)" }} className='sub-banner-container' key={id} onClick={() => {
+                                    handleBanner(index)
+                                }}>
+                                    <img className='sub-banner-img' src={gameImage} alt="" />
+                                </li>
+                            }
+                            else {
+                                return <li className='sub-banner-container' key={id} onClick={() => {
+                                    handleBanner(index)
+                                }}>
+                                    <img className='sub-banner-img' src={gameImage} alt="" />
+                                </li>
+                            }
+                        })}
+                    </ul>
+                </div >
+            </section> : null}
+        </>
     )
 }
 
