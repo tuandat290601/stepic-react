c8e24b1400f58dc9937684693df0018bfd24cda0
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2022-07-09 09:18:56 +0700

    update navbar (hide in login page)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-import {BsSearch, BsGlobe, BsChevronDown, BsPersonFill, BsCartFill} from "react-icons/bs"
+import { BsSearch, BsGlobe, BsChevronDown, BsPersonFill, BsCartFill } from "react-icons/bs"
 import { MdLogout } from "react-icons/md";
 
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
@@ -9,7 +9,8 @@ import { MDBIcon } from 'mdb-react-ui-kit';
 
 import "./Navbar.sass"
 import "../../App.sass"
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentPage } from '../../features/navbar/navbarSlice';
 
 const list = [{
   name: "VN",
@@ -19,83 +20,90 @@ const list = [{
   icon: "gb uk"
 }]
 
+
 const Navbar = () => {
 
-  const {currentUser} = useSelector(store => store.login)
+  const { currentUser } = useSelector(store => store.login)
 
   const [language, setLanguage] = useState("VN")
   const [languageList, setLanguageList] = useState(false)
   const [accountList, setAccountList] = useState(false)
-  console.log(languageList)
+
+  const { currentPage } = useSelector(store => store.navbar)
+  const dispatch = useDispatch();
+
   return (
-    <nav className='navbar'>
-      <div className="logo">
-        <Link to = "/">
-          <img src="/img/logo.png" alt="" />
-        </Link>
-      </div>
-      <ul className="nav-list">
-        <li className='nav-item'>
-          <Link to = "game" className='nav-link'>game</Link>
-        </li>
-        <li className='nav-item'>
-          <Link to = "event" className='nav-link'>event</Link>
-        </li>
-      </ul>
-      <div className="search-container">
-        <input type="text" className='search' placeholder='Search ...' />
-        <div className="search-btn">
-          <BsSearch/>
+    <>
+      {currentPage !== "login" ? <nav className='navbar'>
+        <div className="logo">
+          <Link to="/" >
+            <img src="/img/logo.png" alt="" />
+          </Link>
         </div>
-      </div>
-      <div className="account-container">
-        <div className="language-container" onClick={()=>setLanguageList(!languageList)}>
-          <div className="language-icon">
-            <BsGlobe/>
+        <ul className="nav-list">
+          <li className='nav-item'>
+            <Link to="game" className='nav-link' >game</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to="event" className='nav-link' >event</Link>
+          </li>
+        </ul>
+        <div className="search-container">
+          <input type="text" className='search' placeholder='Search ...' />
+          <div className="search-btn">
+            <BsSearch />
           </div>
-          <div className="current-language" >
-            {language}
-          </div>
-          <div className = {languageList ? "language-icon rotate-180" : "language-icon rotate-0"} >
-            <BsChevronDown/>
-          </div>
-          {languageList ? <ul className="language-sub-list">
-            {list.map((item, index)=>{
-              const {name, icon} = item
-              return <li key={index} onClick={()=>{
-                setLanguage(name)
-                setLanguageList(!languageList)
-              }
-              }>{<MDBIcon flag={icon} />}{name}</li>
-            })}
-          </ul> : null}
         </div>
-        {currentUser !== "" ? <div className="account" onClick={()=>setAccountList(!accountList)}>
-          <div className="account-avatar">
-            <img src="https://znews-stc.zdn.vn/static/topic/person/justin.jpg" alt="" />
+        <div className="account-container" onClick={() => { setLanguageList(!languageList) }}>
+          <div className="language-container" >
+            <div className="language-icon">
+              <BsGlobe />
+            </div>
+            <div className="current-language" >
+              {language}
+            </div>
+            <div className={languageList ? "language-icon rotate-180" : "language-icon rotate-0"} >
+              <BsChevronDown />
+            </div>
+            {languageList ? <ul className="language-sub-list">
+              {list.map((item, index) => {
+                const { name, icon } = item
+                return <li key={index} onClick={() => {
+                  setLanguage(name)
+                  setLanguageList(!languageList)
+                }
+                }>{<MDBIcon flag={icon} />}{name}</li>
+              })}
+            </ul> : null}
           </div>
-          <div className = {accountList ? "account-icon rotate-180" : "account-icon rotate-0"}>
-            <BsChevronDown/>
-          </div>
-          {accountList ? <ul className="account-nav-list">
-            <li>
-              <BsPersonFill/>
-              My account
-            </li>
-            <li>
-              <Link to = "payment">
-                <BsCartFill/>
-                My Cart
-              </Link>
-            </li>
-            <li>
-              <MdLogout/>
-              Sign out
-            </li>
-          </ul> : null}
-        </div> : <Link to = "login" className='account link-to-login'>Sign In</Link>}
-      </div>
-    </nav>
+          {currentUser !== "" ? <div className="account" >
+            <div className="account-avatar">
+              <img src="https://znews-stc.zdn.vn/static/topic/person/justin.jpg" alt="" />
+            </div>
+            <div className={accountList ? "account-icon rotate-180" : "account-icon rotate-0"}>
+              <BsChevronDown />
+            </div>
+            {accountList ? <ul className="account-nav-list">
+              <li>
+                <BsPersonFill />
+                My account
+              </li>
+              <li>
+                <Link to="payment">
+                  <BsCartFill />
+                  My Cart
+                </Link>
+              </li>
+              <li>
+                <MdLogout />
+                Sign out
+              </li>
+            </ul> : null}
+          </div> : <Link to="login" className='account link-to-login' onClick={() => dispatch(setCurrentPage("login"))}>Sign In</Link>}
+        </div>
+      </nav> : <></>}
+    </>
+
   )
 }
 
