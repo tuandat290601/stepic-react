ca9f117e808bd9ad97a3f995e1cb3070468e8203
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2022-07-14 17:43:34 +0700

    create Game page

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Sort, Filter, GameList } from '../../components'
+
+import "./Game.sass"
 
 const Game = () => {
-  return (
-    <div>Game</div>
+  const {filteredProductList} = useSelector(store => store.product)
+  const {searchKey} = useSelector(store => store.navbar)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    if(filteredProductList.length !== 0)  {
+      setLoading(false)
+    }
+  },[filteredProductList])
+
+  return (<>
+  {loading ? null : <div id = "game-page">
+      <div className="game-page-container">
+        <div className="game-page-header">
+          <div className="result-notice">
+            Results for: {searchKey}
+          </div>
+          <Sort/>
+        </div>
+        <hr />
+        <div className="game-page-body">
+          <GameList/>
+          <Filter/>
+        </div>
+      </div>
+    </div>}
+    
+  </>
   )
 }
 
